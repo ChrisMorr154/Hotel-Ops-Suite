47e83025d98f397f88e3511ef54e7be127aa6d2c
Christian Morris <christianmorris154@gmail.com>
2025-11-26 16:40:04 -0800

    Add pages and init logic

--- Hotel-Ops-Suite/ViewModels/Kitchen.xaml.cs
+++ Hotel-Ops-Suite/ViewModels/Kitchen.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Hotel_Ops_Suite.Views;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,15 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hotel_Ops_Suite
+namespace Hotel_Ops_Suite.Views
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for Kitchen.xaml
+    /// </summary>
+    public partial class Kitchen : Page
     {
-        public MainWindow()
+        public Kitchen()
         {
             InitializeComponent();
-            var loginPage = new Login();
-            this.Content = loginPage;
         }
     }
 }
